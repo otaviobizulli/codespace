36e7504aa3058691822f4f4e8ab780b6de4329c7
Otávio Bizulli <102388205+otaviobizulli@users.noreply.github.com>
2023-02-10 01:08:23 +0000

    aula1

---
+++ aula2.java
@@ -0,0 +1,9 @@
+public class aula2 {
+    public static void main(String[] args) {
+        int contador = 0;
+        while(contador < 100) {
+            System.out.println("Bom dia!");
+            contador++;
+    }
+}
+}
