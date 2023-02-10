fe75033fcb3087764bca277fa15633d79ce86f89
Otávio Bizulli <102388205+otaviobizulli@users.noreply.github.com>
2023-02-10 01:27:12 +0000

    ex2

---
+++ ex1.java
@@ -0,0 +1,7 @@
+public class ex1{
+    public static void main(String[] args){
+        String nome;
+        nome = "Otavio Rodrigues Bizulli";
+        System.out.println(nome);
+    }
+}
---
+++ ex2.java
@@ -0,0 +1,11 @@
+public class ex2 {
+    public static void main(String[] args){
+        double nota1;
+        double nota2;
+        double resul;
+        nota1 = 6;
+        nota2 = 6;
+        resul = (nota1 + nota2) / 2;
+        System.out.println(resul);
+    }
+}
