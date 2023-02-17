d269c3b8ee421acf0d9d1a981741084ccfcabce9
Otávio Bizulli <102388205+otaviobizulli@users.noreply.github.com>
2023-02-17 01:22:10 +0000

    Aula2

---
+++ TesteIf.java
@@ -0,0 +1,15 @@
+public class TesteIf
+{
+    public static void main(String[] args)
+    {
+        int preco = 10;
+        if (preco < 0)
+        {
+            System.out.println("Preço negativo!");
+        }
+        else
+        {
+            System.out.println("Produto cadastrado!");
+        }
+    }
+}
---
+++ ex1aula2.java
@@ -0,0 +1,13 @@
+public class ex1aula2
+{
+    public static void main(String[] args)
+    {
+        int cont = 1;
+        while (cont < 101)
+        {
+            System.out.println(cont);
+            cont += 1;
+        }
+    }
+       
+}
